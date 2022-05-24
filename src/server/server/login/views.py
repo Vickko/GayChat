5d4581e8f8c503201044a17d72331dadf9913aca
PauperZ <wddwsz@foxmail.com>
2022-05-24 23:21:58 +0800

    Please stop pushing us

---
+++ src/server/server/login/views.py
@@ -0,0 +1,45 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.contrib.auth.models import User
+from django.contrib import auth
+from rest_framework.authtoken.views import APIView, AuthTokenSerializer
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+
+# 用户注册
+#User.objects.filter(username="Vickko").delete()
+class Signup(APIView):
+    def post(self, request, *args, **kwargs):
+        username = request.data.get('username')
+        password = request.data.get('password')
+        if User.objects.filter(username=username).exists():
+            resp = {
+                'status': False,
+                'data': '用户名已被注册'
+            }
+        else:
+            user = User.objects.create_user(username=username, password=password)
+            token, created = Token.objects.get_or_create(user=user)
+            resp = {
+                'status': True,
+                'token': token.key,
+                'user_id': user.pk,
+                'user_name': user.username,
+            }
+        return Response(resp)
+
+# 用户登录
+class Login(APIView):
+
+    def post(self, request, *args, **kwargs):
+        serializer = AuthTokenSerializer(data=request.data, context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({
+            'status': True,
+            'token': token.key,
+            'user_id': user.pk,
+            'user_name': user.username,
+        })

ed751092ae8442905488470db61414b5208bcbe1
Vickko <vickkochyan@qq.com>
2022-05-25 10:29:00 +0800

    refactor(api): Correct format of Login, Signup data

--- src/server/server/login/views.py
+++ src/server/server/login/views.py
@@ -8,38 +8,56 @@ from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
 
 # 用户注册
-#User.objects.filter(username="Vickko").delete()
+# User.objects.filter(username="Vickko").delete()
+
+
 class Signup(APIView):
     def post(self, request, *args, **kwargs):
         username = request.data.get('username')
         password = request.data.get('password')
         if User.objects.filter(username=username).exists():
             resp = {
-                'status': False,
-                'data': '用户名已被注册'
+                'data': {
+                    'id': '',
+                },
+                'meta': {
+                    'msg': '用户名已被注册',
+                    'status': 422,
+                },
             }
+            return Response(resp,422)
         else:
-            user = User.objects.create_user(username=username, password=password)
-            token, created = Token.objects.get_or_create(user=user)
+            user = User.objects.create_user(
+                username=username, password=password)
             resp = {
-                'status': True,
-                'token': token.key,
-                'user_id': user.pk,
-                'user_name': user.username,
+                'data': {
+                    'id': user.pk,
+                },
+                'meta': {
+                    'msg': '注册成功',
+                    'status': 201,
+                },
             }
-        return Response(resp)
+            return Response(resp)
 
 # 用户登录
+
+
 class Login(APIView):
 
     def post(self, request, *args, **kwargs):
-        serializer = AuthTokenSerializer(data=request.data, context={'request': request})
+        serializer = AuthTokenSerializer(
+            data=request.data, context={'request': request})
         serializer.is_valid(raise_exception=True)
         user = serializer.validated_data['user']
         token, created = Token.objects.get_or_create(user=user)
         return Response({
-            'status': True,
-            'token': token.key,
-            'user_id': user.pk,
-            'user_name': user.username,
+            'data': {
+                'id': user.pk,
+                'token': token.key,
+            },
+            'meta': {
+                'msg': '登录成功',
+                'status': 200,
+            }
         })
